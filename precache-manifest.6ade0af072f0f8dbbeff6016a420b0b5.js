b614ab229a64eb4ba64ab931de3f2fe5787de7ac
worstpractice <54796463+worstpractice@users.noreply.github.com>
2020-09-11 06:34:58 +0200

    Updates

---
+++ precache-manifest.6ade0af072f0f8dbbeff6016a420b0b5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb8d376328b7db21aeb1bb5fb7105105",
+    "url": "/graphql-countries-index/index.html"
+  },
+  {
+    "revision": "127c32f7912980c17bef",
+    "url": "/graphql-countries-index/static/css/2.bb59309b.chunk.css"
+  },
+  {
+    "revision": "45e7ec4a31c4a2df6e1a",
+    "url": "/graphql-countries-index/static/css/main.9aa72871.chunk.css"
+  },
+  {
+    "revision": "127c32f7912980c17bef",
+    "url": "/graphql-countries-index/static/js/2.c8b19eba.chunk.js"
+  },
+  {
+    "revision": "631224be8d33bde78b822b571f1788fb",
+    "url": "/graphql-countries-index/static/js/2.c8b19eba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45e7ec4a31c4a2df6e1a",
+    "url": "/graphql-countries-index/static/js/main.8c960099.chunk.js"
+  },
+  {
+    "revision": "3b2ceec6ff2b08c2f10e",
+    "url": "/graphql-countries-index/static/js/runtime-main.29904532.js"
+  },
+  {
+    "revision": "8dbef9e7809cef03fbdb1ea051128ace",
+    "url": "/graphql-countries-index/static/media/anivia.8dbef9e7.jpg"
+  },
+  {
+    "revision": "a59900550bc37be3cc2781a67bfd0607",
+    "url": "/graphql-countries-index/static/media/ashe.a5990055.jpg"
+  },
+  {
+    "revision": "8a0a03c17e70c296c24ceed172c092c8",
+    "url": "/graphql-countries-index/static/media/logo.8a0a03c1.svg"
+  },
+  {
+    "revision": "b1472d44994d74a83dffbd661d9f0dbf",
+    "url": "/graphql-countries-index/static/media/search.b1472d44.svg"
+  }
+]);
