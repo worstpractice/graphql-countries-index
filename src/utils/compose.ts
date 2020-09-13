be0083977ad786f428b2f48685bad80885822e03
worstpractice <54796463+worstpractice@users.noreply.github.com>
2020-09-14 01:16:48 +0200

    phrasing

--- src/utils/compose.ts
+++ src/utils/compose.ts
@@ -1,5 +1,5 @@
 /** Since we won't be passing the callbacks any arguments, they must not want
- * to be given any in the first place. */
+ * to receive any in the first place. */
 type ZeroParameterCallback = () => void;
 
 /** Composes any number of functions into a single function. */
