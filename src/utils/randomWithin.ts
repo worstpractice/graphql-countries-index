f7300de009aea2edd98ff81d1bb8f56252c1b914
worstpractice <54796463+worstpractice@users.noreply.github.com>
2020-09-14 01:15:35 +0200

    phrasing

--- src/utils/randomWithin.ts
+++ src/utils/randomWithin.ts
@@ -1,4 +1,4 @@
-/** Returns a random integer falling within a provided range. */
+/** Returns a random integer falling within the provided range. */
 export const randomWithin = (range: number) => {
   return Math.floor(Math.random() * range);
 };
