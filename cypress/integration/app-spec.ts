d4487cc8b6f79d3682615a9ddd2bc267713d2e66
worstpractice <54796463+worstpractice@users.noreply.github.com>
2020-09-14 17:22:19 +0200

    add additional cypress tests

--- cypress/integration/app-spec.ts
+++ cypress/integration/app-spec.ts
@@ -7,19 +7,73 @@ describe("App", () => {
   });
 
   it("can search for countries by name", () => {
-    cy.get("[data-cy=TextInput]").click().type("antarctica");
+    cy.get("[data-cy=TextInput]").click().clear().type("antarctica");
 
-    cy.get("[data-cy=ContentCard]").should("exist").and("be.visible");
-    cy.get("[data-cy=Flag-AQ]").should("exist").and("be.visible");
+    cy.get("[data-cy=ContentCard]").should("be.visible");
+    cy.get("[data-cy=Flag-AQ]").should("be.visible");
   });
 
-  it("can click a card to reveal the detail view", () => {
-    cy.get("[data-cy=TextInput]").click().type("sweden");
+  it("can input junk without getting results", () => {
+    cy.get("[data-cy=TextInput]").click().clear().type("narnia");
 
-    cy.get("[data-cy=DetailView]").should("not.exist").and("not.be.visible");
+    cy.get("[data-cy=ContentCard]").should("not.be.visible");
+    cy.get("[data-cy=Flag-AQ]").should("not.be.visible");
+  });
+
+  it("can click a card to open the detail view", () => {
+    cy.get("[data-cy=TextInput]").click().clear().type("sweden");
+
+    cy.get("[data-cy=DetailView]").should("not.be.visible");
 
     cy.get("[data-cy=ContentCard]").click();
 
-    cy.get("[data-cy=DetailView]").should("exist").and("be.visible");
+    cy.get("[data-cy=DetailView]").should("be.visible");
+  });
+
+  it("can exit detail view", () => {
+    cy.get("[data-cy=TextInput]").click().clear().type("antarctica");
+
+    cy.get("[data-cy=DetailView]").should("not.be.visible");
+
+    cy.get("[data-cy=ContentCard]").click();
+
+    cy.get("[data-cy=DetailView]").should("be.visible");
+
+    cy.get("[data-cy=Header]").click({ force: true });
+
+    cy.get("[data-cy=DetailView]").should("not.be.visible");
+  });
+
+  it("can clear all results", () => {
+    cy.get("[data-cy=ContentCard]").should("not.be.visible");
+    cy.get("[data-cy=Flag-AQ]").should("not.be.visible");
+
+    cy.get("[data-cy=TextInput]").click().clear().type("antarctica");
+
+    cy.get("[data-cy=ContentCard]").should("be.visible");
+    cy.get("[data-cy=Flag-AQ]").should("be.visible");
+
+    cy.get("[data-cy=TextInput]").click().clear();
+
+    cy.get("[data-cy=ContentCard]").should("not.be.visible");
+    cy.get("[data-cy=Flag-AQ]").should("not.be.visible");
+  });
+
+  it("can navigate inside the detail view", () => {
+    cy.get("[data-cy=TextInput]").click().clear().type("sweden");
+
+    cy.get("[data-cy=DetailView]").should("not.be.visible");
+
+    cy.get("[data-cy=ContentCard]").click();
+
+    cy.get("[data-cy=DetailView]").should("be.visible");
+
+    cy.get("[data-cy=ContentCard]").should("be.visible");
+
+    let times = 5;
+
+    while (times--) {
+      cy.get("[data-cy=DetailView]").children().find("[data-cy=ContentCard]").first().click();
+    }
   });
 });
